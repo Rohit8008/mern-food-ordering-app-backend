eb72e992d1744e2910968d141d764b857b0b42f2
Rohit8008 <mittalrohit701@gmail.com>
2025-01-04 22:56:56 +0530

    added details page and cart

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -4,16 +4,27 @@ import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
 
+//api/restaurant/restaurant1
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId param must be a valid String"),
+    RestaurantController.getRestaurant
+);
 
 // /api/restaurant/search/london
-router.get("/search/:city",
-    param("city")
+router.get(
+  "/search/:city",
+  param("city")
     .isString()
     .trim()
     .notEmpty()
     .withMessage("City param must be a valid String"),
 
-    RestaurantController.searchRestaurant
+  RestaurantController.searchRestaurant
 );
 
 export default router;
