aaabbb2131e84643e4c18bd87382e2b4f6718c69
Rohit8008 <rohitmittal137@gmail.com>
2025-01-07 21:18:14 +0530

    adding view order

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -1,4 +1,3 @@
-
 import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
@@ -9,30 +8,46 @@ const router = express.Router();
 
 const storage = multer.memoryStorage();
 const upload = multer({
-    storage:storage,
-    limits:{
-        fileSize: 5 * 1024 * 1024 //5mb
-    }
-})
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
 
 // /api/my/restaurant
 
-router.get("/",jwtCheck,jwtParse,MyRestaurantController.getMyRestaurant);
-
-router.post("/",
-    upload.single("imageFile"),
-    validateMyRestaurantRequest,
-    jwtCheck,
-    jwtParse,
-    MyRestaurantController.createMyRestaurant
+router.get(
+  "/order",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.getMyRestaurantOrders
 );
 
-router.put("/",
-    upload.single("imageFile"),
-    validateMyRestaurantRequest,
-    jwtCheck,
-    jwtParse,
-    MyRestaurantController.updateMyRestaurant
-)
+router.patch(
+  "/order/:orderId/status",
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateOrderStatus
+);
+
+router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
+
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant
+);
+
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
 
 export default router;
--- src/routes/OrderRoute.ts
+++ src/routes/OrderRoute.ts
@@ -1,13 +1,18 @@
-import  express  from "express";
+import express from "express";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import OrderController from "../controllers/OrderController";
 
 const router = express.Router();
 
-router.post("/checkout/create-checkout-session",jwtCheck,jwtParse,
-    OrderController.createCheckoutSession
+router.get("/",jwtCheck,jwtParse,OrderController.getMyOrders);
+
+router.post(
+  "/checkout/create-checkout-session",
+  jwtCheck,
+  jwtParse,
+  OrderController.createCheckoutSession
 );
 
-router.post("/checkout/webhook",OrderController.stripeWebhookHandler)
+router.post("/checkout/webhook", OrderController.stripeWebhookHandler);
 
 export default router;
